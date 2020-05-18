d6eb2ee94688f7191713961e109b65642ba45fe9
Gabriela Menkiewicz <akatsuki.no.razu@gmail.com>
2020-05-18 12:59:12 +0200

    fix run/pause/reset logic, add new start port generation

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { Matrix, calculateNextGeneration, generateStartPoint } from '../services/GameLogic';
 
@@ -6,24 +6,26 @@ type Params = {
   startPoint: string,
 };
 
-const startPointSize = 50; //prepare table to overflow instead if squish
+const startPointSize = 20; //prepare table to overflow instead if squish
 
 const Game: React.FC<RouteComponentProps<Params>> = ({ match }) => {
-  let parsedURL: Matrix | undefined;
+  let parsedUrl: Matrix | undefined;
   let parseError: string;
   try {
-    parsedURL = match.params && match.params.startPoint && JSON.parse(match.params.startPoint)
+    parsedUrl = match.params && match.params.startPoint && JSON.parse(match.params.startPoint)
   } catch {
     parseError = 'There’s a typo in the starting point. Please make sure it looks like this: [[0,1,0],[0,1,0],[0,1,0]]'; //show it somewhere
   }
 
-  const startPoint = parsedURL ? parsedURL : generateStartPoint(startPointSize, startPointSize);
+  const startPoint = parsedUrl ? parsedUrl : generateStartPoint(startPointSize, startPointSize);
   const gameUrl = useRef<HTMLInputElement>(null);
 
   const [area, setArea] = useState(startPoint);
+  const [currentStartPoint, setCurrentStartPoint] = useState(startPoint);
+  const [shareUrl, setShareUrl] = useState(window.location.href);
   const [generation, setGeneration] = useState(1);
   const [ticker, setTicker] = useState(1);
-  const [paused, setPaused] = useState(false);
+  const [paused, setPaused] = useState(true);
 
   const setup = ((game: Matrix) => {
     const interval: number = window.setInterval(() => {
@@ -36,30 +38,51 @@ const Game: React.FC<RouteComponentProps<Params>> = ({ match }) => {
     return () => clearInterval(interval);
   });
 
-  const copyUrl = () => {
+  const copyUrl = (): void => {
     gameUrl.current && gameUrl.current.select();
     document.execCommand("copy");
   }
 
-
-  const toggleTicking = () => {
+  const toggleTicking = (): void => {
     paused ? setup(area) : clearInterval(ticker);
     setPaused(!paused);
   }
 
-  const reset = () => {
+  const reset = (newStartPoint?: Matrix): void => {
     clearInterval(ticker);
-    setArea(startPoint);
+    if (newStartPoint) setCurrentStartPoint(newStartPoint);
+    setArea(newStartPoint ? newStartPoint : currentStartPoint);
     setGeneration(1);
-    setup(startPoint);
+    if (!paused) setup(newStartPoint ? newStartPoint : currentStartPoint);
   }
+
+  // useEffect(() => {
+  //   // console.log('generation', generation);
+
+  // }, [generation]);
+
+  // useEffect(() => {
+  //   // console.log('area', JSON.stringify(area));
+  //   // [[0,0,0,1,0],[1,0,1,0,1],[0,0,1,1,1],[0,1,0,0,0],[0,0,1,1,0]]
+  // }, [area]);
+
+  useEffect(() => {
+    // console.log('newStartPoint', JSON.stringify(currentStartPoint));
+    // console.log('href', window.location.origin);
+    setShareUrl(`${window.location.origin}/${JSON.stringify(currentStartPoint)}`);
+  }, [currentStartPoint]);
 
   return (
     <header className="App-header">
       <section>
         <div>
-          <button onClick={reset} className="toggle">{generation > 1 ? 'Reset' : 'Run'}</button>
-          <button onClick={toggleTicking} className="toggle">{paused ? 'Resume' : 'Pause'}</button>
+          <p>
+            <button onClick={() => reset(generateStartPoint(startPointSize, startPointSize))} className="toggle">New start point</button>
+          </p>
+        </div>
+        <div>
+          <button onClick={() => reset()} className="toggle" disabled={generation <= 1}>Reset</button>
+          <button onClick={toggleTicking} className="toggle">{paused ? (generation > 1 ? 'Resume' : 'Run') : 'Pause'}</button>
         </div>
         <p>
           Current generation: {generation}
@@ -84,7 +107,7 @@ const Game: React.FC<RouteComponentProps<Params>> = ({ match }) => {
       <section>
         <p><small>Have you enjoyed this particular game? <br />Share it with your peers using the link below:</small></p>
         <div>
-          <input ref={gameUrl} value={window.location.href} readOnly />
+          <input ref={gameUrl} value={shareUrl} readOnly />
           <button onClick={copyUrl} className="copy">Copy</button>
         </div>
       </section>
